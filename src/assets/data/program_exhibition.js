40d919b3a84fa69f83f296e4ba89f72cd661da47
Hyungjoong Kim <hjk@Hyungjoongs-MacBook-Air.local>
2026-06-01 01:00:47 +0900

    aa

--- src/assets/data/program_exhibition.js
+++ src/assets/data/program_exhibition.js
@@ -39,14 +39,14 @@ export const programExhibition = {
       artist: { kr: '투카 라우리라', en: 'Tuukka Laurila' },
       title: { kr: '임시 스튜디오', en: 'Temporary Studio' },
       description: {
-        kr: `투카 라우리라의 공개 워크숍은 현장에서 수집한 폐건축 자재로 구축하는 ‘살아있는 공간'이라는 프로젝트의 일환으로, 2일간 진행됩니다. 
+        kr: `투카 라우리라의 프로젝트 '임시 스튜디오'는 현장에서 수집한 폐건축 자재로 구축하는 ‘살아있는 공간'이라는 프로젝트의 일환으로, 이는 페스티벌의 전 기간에 걸쳐 이루어집니다. 
 
-참여자들은 함께 자재를 모으고 분류하고 쌓아 나가면서 임시 구조물을 만듭니다. 이렇게 만들어진 구조물은 변형 가능한 것으로, 최종적으로는 재활용될 것입니다.
+참여자들은 그와 함께 하는 3일간의 오픈 워크숍을 통해 라우리라와 함께 자재를 모으고 분류하고 쌓아 나가면서 임시 구조물을 만듭니다. 이렇게 만들어진 구조물은 변형 가능한 것으로, 최종적으로는 재활용될 것입니다.
 
 라우리라에게 있어, 작업이란 곧 과정입니다. 구조물 짓기라는 행위와 그 속에서 일어나는 교류를 통해 형성되는 임시 공동체는 오늘날 물질주의적인 세상이 추구하는 끊임없는 새로움에 맞선 저항의 실마리를 제시하고 있습니다.`.trim(),
-        en: `Open workshop sessions at SA-I Festival with Paris-based artist Tuukka Laurila will take place over two days as part of an ongoing "living environment" constructed from locally sourced found and discarded building materials.
-
-Throughout the workshop, materials will be collectively gathered, sorted, and gradually assembled into temporary structures that remain open to modification, and in the end, return to existing cycles of reuse.
+        en: `The project <i>Temporary Studio</i> by Paris-based artist Tuukka Laurila is an ongoing project that constructs a "living environment" from locally sourced found and discarded building materials. It takes place over the entire duration of the festival. 
+        
+Participants will work together with Laurila over three days of open workshops to gather, sort, and gradually assemble materials into temporary structures that remain open to modification, and in the end, return to existing cycles of reuse.
 
 For Laurila, the process is the work: a temporary community formed through building and exchange, within a space that is a proposition against the perpetual newness of today's material world.`.trim(),
       },
--- src/assets/data/program_workshop_schedule.js
+++ src/assets/data/program_workshop_schedule.js
@@ -119,13 +119,13 @@ Participants are recommended to bring a musical instrument to the workshop, whic
     artist: { kr: '투카 라우리라', en: 'Tuukka Laurila' },
     title: { kr: '오픈 워크숍: 임시 스튜디오', en: 'OPEN WORKSHOP: TEMPORARY STUDIO' },
     description: {
-      kr: `투카 라우리라의 공개 워크숍은 현장에서 수집한 폐건축 자재로 구축하는 ‘살아있는 공간'이라는 프로젝트의 일환으로, 2일간 진행됩니다. 
+      kr: `투카 라우리라의 공개 워크숍은 현장에서 수집한 폐건축 자재로 구축하는 ‘살아있는 공간'이라는 프로젝트의 일환으로, 3일간 진행됩니다. 
 
 참여자들은 함께 자재를 모으고 분류하고 쌓아 나가면서 임시 구조물을 만듭니다. 이렇게 만들어진 구조물은 변형 가능한 것으로, 최종적으로는 재활용될 것입니다.
 
 라우리라에게 있어, 작업이란 곧 과정입니다. 구조물 짓기라는 행위와 그 속에서 일어나는 교류를 통해 형성되는 임시 공동체는 오늘날 물질주의적인 세상이 추구하는 끊임없는 새로움에 맞선 저항의 실마리를 제시하고 있습니다.  
 `.trim(),
-      en: `Open workshop sessions at SA-I Festival with Paris-based artist Tuukka Laurila will take place over two days as part of an ongoing "living environment" constructed from locally sourced found and discarded building materials.
+      en: `Open workshop sessions at SA-I Festival with Paris-based artist Tuukka Laurila will take place over three days as part of an ongoing "living environment" constructed from locally sourced found and discarded building materials.
 
 Throughout the workshop, materials will be collectively gathered, sorted, and gradually assembled into temporary structures that remain open to modification, and in the end, return to existing cycles of reuse.
 
@@ -138,13 +138,13 @@ For Laurila, the process is the work: a temporary community formed through build
     artist: { kr: '투카 라우리라', en: 'Tuukka Laurila' },
     title: { kr: '임시 스튜디오 (1부)', en: 'TEMPORARY STUDIO (SESSION 1)' },
     description: {
-      kr: `투카 라우리라의 공개 워크숍은 현장에서 수집한 폐건축 자재로 구축하는 ‘살아있는 공간'이라는 프로젝트의 일환으로, 2일간 진행됩니다. 
+      kr: `투카 라우리라의 공개 워크숍은 현장에서 수집한 폐건축 자재로 구축하는 ‘살아있는 공간'이라는 프로젝트의 일환으로, 3일간 진행됩니다. 
 
 참여자들은 함께 자재를 모으고 분류하고 쌓아 나가면서 임시 구조물을 만듭니다. 이렇게 만들어진 구조물은 변형 가능한 것으로, 최종적으로는 재활용될 것입니다.
 
 라우리라에게 있어, 작업이란 곧 과정입니다. 구조물 짓기라는 행위와 그 속에서 일어나는 교류를 통해 형성되는 임시 공동체는 오늘날 물질주의적인 세상이 추구하는 끊임없는 새로움에 맞선 저항의 실마리를 제시하고 있습니다.  
 `.trim(),
-      en: `Open workshop sessions at SA-I Festival with Paris-based artist Tuukka Laurila will take place over two days as part of an ongoing "living environment" constructed from locally sourced found and discarded building materials.
+      en: `Open workshop sessions at SA-I Festival with Paris-based artist Tuukka Laurila will take place over three days as part of an ongoing "living environment" constructed from locally sourced found and discarded building materials.
 
 Throughout the workshop, materials will be collectively gathered, sorted, and gradually assembled into temporary structures that remain open to modification, and in the end, return to existing cycles of reuse.
 
